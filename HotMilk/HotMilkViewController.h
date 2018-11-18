e1b236da67e873849011d52c41a29f53170964d9
renyida <renyida@dangdang.com>
2018-11-19 00:00:28 +0800

    init project

---
+++ HotMilk/HotMilkViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotMilkViewController.h
+//  HotMilk
+//
+//  Created by radar on 2018/11/18.
+//  Copyright © 2018年 radar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotMilkViewController : UIViewController
+
+@end
---
+++ HotMilk/HotMilkViewController.m
@@ -0,0 +1,27 @@
+//
+//  HotMilkViewController.m
+//  HotMilk
+//
+//  Created by radar on 2018/11/18.
+//  Copyright © 2018年 radar. All rights reserved.
+//
+
+#import "HotMilkViewController.h"
+
+@interface HotMilkViewController ()
+
+@end
+
+@implementation HotMilkViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+    self.navigationItem.title = @"HOT MILK";
+    
+    
+}
+
+
+@end
