07bdf338740cbd2cd48d2d57ce427f2c062388a5
renyida <renyida@dangdang.com>
2018-11-19 00:32:14 +0800

    add pch file

--- HotMilk/HotMilkViewController.m
+++ HotMilk/HotMilkViewController.m
@@ -8,7 +8,7 @@
 
 #import "HotMilkViewController.h"
 
-@interface HotMilkViewController ()
+@interface HotMilkViewController () <RDTableViewDelegate>
 
 @end
 
@@ -20,7 +20,15 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.title = @"HOT MILK";
     
-    
+    //添加主列表
+//    RDTableView *table = [[RDTableView alloc] initWithFrame:CGRectMake(0, 0, 320, 230)];
+//    table.delegate = self;
+//    table.tag = 1000;
+//    table.loadMoreStyle = LoadMoreStyleLoading;
+//    table.refreshStyle = RefreshStyleDropdown;
+//    table.multiSelectEnabled = YES;
+//    table.editRowEnabled = NO;
+//    table.moveRowEnabled = NO;
 }
 
 
